81ddcb080ec1cb863b7b254f5bbde81a8b9a033b
onizzm <66412890+onizzm@users.noreply.github.com>
2024-10-09 14:26:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+var portfolio = 0;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function animation() {
+		var bio = document.getElementById("bio");
+		var baniere = document.getElementById("baniere");
+		/*var body = document.getElementByName("body")*/
+		var video = document.getElementById("video");
+		var titre = document.getElementById("titre");
+		var marge = document.getElementById("marge");
+
+
+		if (bio.style.opacity == 1){
+			bio.style.opacity = 0;
+			baniere.style.opacity = 0;
+			titre.style.top = "0vh"
+			await sleep(1000);
+			bio.style.display = "none";
+			marge.style.display = "none";
+			baniere.style.display = "none";
+			/*baniere.style.display = "none";*/
+
+			/*document.body.style.backgroundImage = "url('image/background_port.png')";*/
+			video.style.display = "flex";
+			await sleep(400);
+			video.style.opacity = 1;
+			document.getElementById("titre_bouton").innerHTML = "AFFICHER BIO";
+
+
+		} else {
+			video.style.opacity = 0;
+			titre.style.top = "30vh";
+			await sleep(500);
+			video.style.display = "none";
+			marge.style.display = "block";
+			bio.style.display = "flex";
+			baniere.style.display = "flex";
+			/*baniere.style.display = "flex";*/
+			await sleep(400);
+			baniere.style.opacity = 1;
+			bio.style.opacity = 1;
+			document.body.style.backgroundImage = "none";
+			document.getElementById("titre_bouton").innerHTML = "DECOUVRIR LE PORTFOLIO";
+		}	
+
+}
+
+function next_video() {
+	var video1 = document.getElementById("video1");
+	var video2 = document.getElementById("video2");
+}
+
+
+function loader() {
+	console.log('fonction loader'); 
+
+	const loader = document.getElementById("loader");
+	loader.style.display = 'none';
+}
